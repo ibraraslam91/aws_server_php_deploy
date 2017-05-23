67e183b7ee3581d98f7a3295aa289435e8b6e577
Unknown <ibraraslam91@yahoo.com>
2017-05-23 11:37:31 +0500

    commit

--- propertyR.php
+++ propertyR.php
@@ -114,7 +114,7 @@ function translate_to_en_func($text){
 
 function removeRow($rowID){
     global $mysqli;
-    $query1 = "DELETE FROM table_property WHERE id=$rowID;";
+    $query1 = "DELETE FROM table_property_R WHERE id=$rowID;";
     $result = $mysqli->query($query1) or die($mysqli->error.__LINE__);
 }
 
